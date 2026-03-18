61ed8e5737c65160b58ca1e0a4b894875f252b23
daaron678 <dylanaaron@ufl.edu>
2026-03-18 09:33:25 -0400

    Sorting algorithms

---
+++ main.py
@@ -0,0 +1,22 @@
+from array import array
+import numpy as np
+# use numpy.array?
+
+# helper function to merge together the subarrays
+def merge():
+    pass
+
+def mergeSortInt(arr):
+   pass
+
+def quickSortInt(type):
+    pass
+
+def main():
+    nums = [9,8,6,5,7,3,1,2]
+    print(array.typecode)
+    # mergeSortInt()
+
+
+if __name__ == '__main__':
+    main()
